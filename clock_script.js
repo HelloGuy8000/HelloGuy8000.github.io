5dd4fd0c5cbdfe2427bae10623fe002752dfef97
HelloGuy8000 <140442429+HelloGuy8000@users.noreply.github.com>
2024-06-24 20:15:56 -0400

    Create clock_script.js

---
+++ clock_script.js
@@ -0,0 +1,21 @@
+function setClock() {
+    const now = new Date();
+    const secondHand = document.getElementById('second');
+    const minuteHand = document.getElementById('minute');
+    const hourHand = document.getElementById('hour');
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondDegree = ((seconds / 60) * 360) + 90;
+    const minuteDegree = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const hourDegree = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setClock, 1000);
+setClock();
